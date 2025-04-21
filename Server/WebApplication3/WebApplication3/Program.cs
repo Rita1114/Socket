8be146ad6034e117ebb224d8a84e6f62463100d3
黃靖雲 <alley8332250@fiami.com.tw>
2025-04-21 16:33:17 +0800

    [Update]重新整理資料夾
    1.新增Server資料夾

---
+++ Server/WebApplication3/WebApplication3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Server server = new Server();
+        server.StartServer();
+        Console.ReadLine();
+    }
+}
+public class Server
+{
+    TcpListener listener = new TcpListener(IPAddress.Any, 8888);
+    List<TcpClient> clients = new List<TcpClient>();
+
+    public void StartServer()
+    {
+        listener.Start();
+        Console.WriteLine("Server started...");
+        Task.Run(() =>
+        {
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                clients.Add(client);
+                Console.WriteLine($"{client}已連線");
+                HandleClient(client);
+            }
+        });
+    }
+
+    private async void HandleClient(TcpClient client)
+    {
+        var stream = client.GetStream();
+        var buffer = new byte[4096];
+
+        while (client.Connected)
+        {
+            int len = await stream.ReadAsync(buffer, 0, buffer.Length);
+            var packet = Packet.Deserialize(buffer.Take(len).ToArray());
+
+           Console.WriteLine($"{packet.userName}:{packet.message}");
+
+            foreach (var c in clients)
+            {
+                if (c.Connected)
+                {
+                    await c.GetStream().WriteAsync(buffer, 0, len);
+                }
+            }
+        }
+    }
+}
